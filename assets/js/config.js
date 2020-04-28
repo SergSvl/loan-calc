dbd6164d0c87887020074ae471ff7e91d77e134e
Sergey <srgsvl@list.ru>
2020-04-28 21:24:41 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,8 +4,7 @@ var user_config = {
   companyPayment: 10.5, // 10,5% - процент выплаты компании
   annualService: 10000, // сумма годового обслуживания
   returnDate: 28, // предыдущее число выплаты, если клиет указал 1-е число
-  baseURL: 'http://jantrish/mail', // URL домена с почтовым скриптом, необходим для отправки писем!! Протокол https для писем важен, как и для домена!
-  // baseURL: 'https://zk35.club/mail', // URL домена с почтовым скриптом, необходим для отправки писем!! Протокол https для писем важен, как и для домена!
+  baseURL: 'https://zk35.club/mail', // URL домена с почтовым скриптом, необходим для отправки писем!! Протокол https для писем важен, как и для домена!
   subFolder: '', // имя подпапки, он же ID клиента, писать БЕЗ СЛЭША перед ним, например: 'id384'. Если подпапка abc 2-го уровня, то вписать 'id384/abc'. Для корневой папки домена - оставить "пусто": ''.
   redirectWait: 6, // время задается в секундах
   redirectURL: '/', // любой полный URL
@@ -32,9 +31,5 @@ var user_config = {
       telegram_token: '1072924358:AAFTZX4qXDRiP7Pxqe-Lwd7wPUeqxhIFFVA', // токен от @BotFather
       telegram_chat_id: '-390436489', // chat_id для телеграм
     },
-    // {
-    //   telegram_token: '927652360:AAG0y9YLXIMnE2KK1y_g0lBHFJwTcs3V8ek', // токен от @BotFather
-    //   telegram_chat_id: '-1001336455697', // chat_id для телеграм
-    // },
   ]
 }
